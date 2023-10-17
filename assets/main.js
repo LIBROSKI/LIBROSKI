342493d959b831f5ed0806b9fb5800c75c4b1af8
LIBROSKI <82524690+LIBROSKI@users.noreply.github.com>
2023-10-17 23:17:13 +0200

    JS basic functions

---
+++ assets/main.js
@@ -0,0 +1,26 @@
+const dataContainer = document.getElementById("container");
+
+const fullPopUpContainer = document.getElementById("overlay-container");
+const fullPopUpText = document.getElementById("overlay-value");
+
+function SetPageAbout() {
+    dataContainer.innerHTML = 
+    `
+    <h1>About</h1>
+    <p>Lorem ipsum ipsum lorem florem lorem ipsum lorem</p>
+    `;
+}
+
+window.onload = (event) => {
+    SetPageAbout();
+    console.log(window.navigator.language);
+    if (window.navigator.language != "en-EN") {
+        PopUp("full", "You are using non-standard language. This website was originally written in English.");
+    }
+};
+
+function PupUp(type, value) {
+    console.log(type, value);
+}
+
+// localStorage.setItem("", "")
