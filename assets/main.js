0ed8cf65e403d7fac0db41095508a295ee9259a3
LIBROSKI <82524690+LIBROSKI@users.noreply.github.com>
2023-10-18 13:51:37 +0200

    main

--- assets/main.js
+++ assets/main.js
@@ -2,10 +2,12 @@ const dataContainer = document.getElementById("container");
 
 const fullPopUpContainer = document.getElementById("overlay-container");
 const fullPopUpText = document.getElementById("overlay-value");
+const fullPopUpIcon = document.getElementById("overlay-icon");
 
-function PopUp(value) {
+function PopUp(value, iconName) {
         fullPopUpContainer.style.display = "flex";
         fullPopUpText.innerText = value;
+        fullPopUpIcon.src = `assets/icons/${iconName}.svg`;
 }
 
 // localStorage.setItem("", "")
@@ -14,11 +16,12 @@ window.onload = (event) => {
     SetPageAbout();
     console.log(window.navigator.language);
     if (window.navigator.language != "en-EN") {
-        PopUp("You are using non-standard language. This website was originally written in English.");
+        PopUp("You are using non-standard language. This website was originally written in English." , "light");
         dataContainer.innerHTML = 
         `
 
         `;
+        localStorage.setItem("popup-lang-clicked", "true")
     }
 };
 
